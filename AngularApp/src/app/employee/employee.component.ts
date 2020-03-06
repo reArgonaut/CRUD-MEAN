9720904fd2cd902dca6f32994e12f6473428f405
reArgonaut <adrianyoop@gmail.com>
2020-03-06 07:37:51 -0700

    Primer commit

---
+++ AngularApp/src/app/employee/employee.component.ts
@@ -0,0 +1,73 @@
+import { Component, OnInit } from '@angular/core';
+import { NgForm } from '@angular/forms';
+
+import { EmployeeService } from '../shared/employee.service';
+import { Employee } from '../shared/employee.model';
+
+declare var M: any;
+
+@Component({
+  selector: 'app-employee',
+  templateUrl: './employee.component.html',
+  styleUrls: ['./employee.component.css'],
+  providers: [EmployeeService]
+})
+export class EmployeeComponent implements OnInit {
+
+  constructor(private employeeService: EmployeeService) { }
+
+  ngOnInit() {
+    this.resetForm();
+    this.refreshEmployeeList();
+  }
+
+  resetForm(form?: NgForm) {
+    if (form)
+      form.reset();
+    this.employeeService.selectedEmployee = {
+      _id: "",
+      name: "",
+      position: "",
+      office: "",
+      salary: null
+    }
+  }
+
+  onSubmit(form: NgForm) {
+    if (form.value._id == "") {
+      this.employeeService.postEmployee(form.value).subscribe((res) => {
+        this.resetForm(form);
+        this.refreshEmployeeList();
+        M.toast({ html: 'Guardado correctamente!', classes: 'rounded' });
+      });
+    }
+    else {
+      this.employeeService.putEmployee(form.value).subscribe((res) => {
+        this.resetForm(form);
+        this.refreshEmployeeList();
+        M.toast({ html: 'Actualizado exitosamente!', classes: 'rounded' });
+      });
+    }
+  }
+
+  refreshEmployeeList() {
+    this.employeeService.getEmployeeList().subscribe((res) => {
+      this.employeeService.employees = res as Employee[];
+    });
+  }
+
+  onEdit(emp: Employee) {
+    this.employeeService.selectedEmployee = emp;
+  }
+
+  onDelete(_id: string, form: NgForm) {
+    if (confirm('Estas seguro de borrar el elemento?') == true) {
+      this.employeeService.deleteEmployee(_id).subscribe((res) => {
+        this.refreshEmployeeList();
+        this.resetForm(form);
+        M.toast({ html: 'Borrado exitosamente!', classes: 'rounded' });
+      });
+    }
+  }
+
+}
---
+++ NodeJS/db.js
@@ -0,0 +1,15 @@
+const mongoose = require('mongoose');
+
+mongoose.connect('mongodb+srv://Adrian:adrian@todo-py3xn.mongodb.net/test', (err) => {
+    if (!err)
+        console.log('MongoDB connection succeeded.');
+    else
+        console.log('Error in DB connection : ' + JSON.stringify(err, undefined, 2));
+});
+
+module.exports = mongoose;
+
+
+
+
+
